4709da62ae2ecc9ecea35bdd63de8b2f00943df5
Ambar-07 <ambargairola83@gmail.com>
2025-01-24 00:00:22 +0530

    new

---
+++ basic/main.java
@@ -0,0 +1,8 @@
+class main
+{
+	public static void main (String[] args)
+	{
+	    // Remove ____ and add "I love Java"
+		System.out.println("I love java");
+	}
+}
---
+++ basic/print.java
@@ -0,0 +1,7 @@
+class print{
+	public static void main (String[] args)
+	{
+		System.out.println(4 + 3);
+		System.out.println(2 + 1);
+	}
+}
