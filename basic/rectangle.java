038ff673da8c6fe67d1194b775c2532d94d31c08
Ambar-07 <ambargairola83@gmail.com>
2025-01-24 00:03:04 +0530

    new

---
+++ basic/rectangle.java
@@ -0,0 +1,9 @@
+class rectangle
+{
+	public static void main (String[] args)
+	{
+		System.out.println("area of rectangle is : " + 11 * 13);
+		
+		System.out.println("perimeter of rectangle is " +2 * (11 + 13));
+	}
+}
